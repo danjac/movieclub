85f4c1406432b7b86b568d7f093b6a1a2823bae1
Dan Jacob <danjac2018@gmail.com>
2024-02-06 23:10:11 +0200

    make_signature() function

--- movieclub/activitypub/http_signature.py
+++ movieclub/activitypub/http_signature.py
@@ -1,5 +1,13 @@
+import datetime
+from base64 import b64encode
+from urllib.parse import urlparse
+
 from Crypto import Random
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from django.utils import timezone
+from django.utils.http import http_date
 
 
 def create_key_pair() -> tuple[str, str]:
@@ -9,3 +17,36 @@ def create_key_pair() -> tuple[str, str]:
         key.export_key().decode("utf-8"),
         key.public_key().export_key().decode("utf-8"),
     )
+
+
+def make_signature(
+    destination_url: str,
+    private_key: str,
+    object_id: str,
+    method: str,
+    date: datetime.datetime | None = None,
+) -> str:
+    """Creates a signature from private key."""
+
+    url = urlparse(destination_url)
+    date = date or timezone.now()
+
+    signature_headers = {
+        "(request-target)": f"{method} {url.path}",
+        "host": f"{url.netloc}",
+        "date": http_date(date.timestamp()),
+    }
+
+    message = "\n".join([f"{k}: {v}" for k, v in signature_headers.items()])
+
+    signer = pkcs1_15.new(RSA.import_key(private_key))
+    signed_message = signer.sign(SHA256.new(message.encode("utf-8")))
+
+    signature = {
+        "keyId": f"{object_id}/#main_key",
+        "algorithm": "rsa-sha256",
+        "headers": " ".join(signature_headers.keys()),
+        "signature": b64encode(signed_message).decode("utf-8"),
+    }
+
+    return ",".join(f'{k}="{v}"' for (k, v) in signature.items())
