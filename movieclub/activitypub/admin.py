9ba6a49176b223d8a2f5d6bb983705a97a4a091c
Dan Jacob <danjac2018@gmail.com>
2024-02-08 20:37:25 +0200

    Add Admin for activitypub models

---
+++ movieclub/activitypub/admin.py
@@ -0,0 +1,21 @@
+from typing import ClassVar
+
+from django.contrib import admin
+
+from movieclub.activitypub.models import Actor, Instance
+
+
+@admin.register(Instance)
+class InstanceAdmin(admin.ModelAdmin):
+    """Admin for Instance."""
+
+    list_display: ClassVar = ["domain", "local", "block"]
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Admin for Instance."""
+
+    list_display: ClassVar = ["handle", "instance", "user"]
+    list_select_related: ClassVar = True
+    raw_id_fields: ClassVar = ["instance", "user"]
