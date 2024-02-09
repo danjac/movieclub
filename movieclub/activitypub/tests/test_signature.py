b1f26f3be64fea4f25c9d4a12ef0da4c370762e5
Dan Jacob <danjac2018@gmail.com>
2024-02-09 18:12:17 +0200

    Digest content

--- movieclub/activitypub/tests/test_signature.py
+++ movieclub/activitypub/tests/test_signature.py
@@ -24,5 +24,19 @@ def test_make_signature_with_digest():
         "https://example.com",
         private_key=priv_key,
         object_id=uuid.uuid4().hex,
-        digest=make_digest({}),
+        digest=make_digest(
+            {
+                "@context": "https://www.w3.org/ns/activitystreams",
+                "type": "Person",
+                "id": "https://social.example/alyssa/",
+                "name": "Alyssa P. Hacker",
+                "preferredUsername": "alyssa",
+                "summary": "Lisp enthusiast hailing from MIT",
+                "inbox": "https://social.example/alyssa/inbox/",
+                "outbox": "https://social.example/alyssa/outbox/",
+                "followers": "https://social.example/alyssa/followers/",
+                "following": "https://social.example/alyssa/following/",
+                "liked": "https://social.example/alyssa/liked/",
+            }
+        ),
     )
