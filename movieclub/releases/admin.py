b8f1ad4cc03b0195ef6d8eb0446a13248be5d62d
Dan Jacob <danjac2018@gmail.com>
2024-02-20 19:56:00 +0200

    Tidy up

--- movieclub/releases/admin.py
+++ movieclub/releases/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from typing import ClassVar
+
+from django.contrib import admin
+
+from movieclub.releases.models import Genre, Release
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Admin for Genre model."""
+
+
+@admin.register(Release)
+class ReleaseAdmin(admin.ModelAdmin):
+    """Admin for Release model."""
+
+    list_filter: ClassVar = ["category"]
