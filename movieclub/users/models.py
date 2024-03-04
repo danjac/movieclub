c3dc3ce561bffee5484d3bef7c61f93f6d6f0a44
Dan Jacob <danjac2018@gmail.com>
2024-03-04 17:15:25 +0200

    Add more user fields

--- movieclub/users/models.py
+++ movieclub/users/models.py
@@ -1,11 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-
-if TYPE_CHECKING:  # pragma: no cover
-    from django.db import models
+from django.db import models
 
 
 class UserManager(BaseUserManager):
@@ -45,4 +41,19 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     """Custom User model."""
 
+    bio = models.TextField(blank=True)
+
     objects: models.Manager[User] = UserManager()
+
+
+class Link(models.Model):
+    """Link to user's webpage or social media etc."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="links")
+
+    title = models.CharField(max_length=12)
+    url = models.URLField()
+
+    def __str__(self) -> str:
+        """Return the URL"""
+        return self.url
